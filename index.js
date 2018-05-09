e0ecd6fd451f8ab61a5e8b5e21bd0f6e39a8ee10
Brad Carlson <b2carlso@yahoo.com>
2018-05-09 16:46:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,15 @@ class BoardMember {
     return "You can do that!"
   }
 
+  doCharity() {
+    return "I like to help people."
+  }
 
+  releasePressStatement() {
+    return "You will see great things from Scruber."
+  }
+
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
 }
