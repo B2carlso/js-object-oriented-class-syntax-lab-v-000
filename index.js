60f9b99591603491757ce0bbac22177ba727d2a0
Brad Carlson <b2carlso@yahoo.com>
2018-05-09 16:47:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ class BoardMember {
   sayHi() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
+
+  sayHello() {
+    return "Welcome aboard!"
+  }
 }
 
 class Ceo extends BoardMember {
